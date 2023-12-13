f40ad69b94a02783e982366a00ca7f48ecce5b33
zxy <zxy06291@gmail.com>
2023-12-14 02:56:16 +0800

    1.制作Loading组件，内部用jquery blockui插件实现

---
+++ admin/public/static/js/loading.js
@@ -0,0 +1,20 @@
+// 显示等待框
+Loading = {
+  show: function () {
+    $.blockUI({
+      message: '<img src="/static/image/loading.gif" />',
+      css: {
+        padding: "10px",
+        left: "50%",
+        width: "80px",
+        marginLeft: "-40px",
+      }
+    });
+  },
+  hide: function () {
+    // 本地查询速度太快，loading显示一瞬间，故意做个延迟，其实这里面的内容只需要$.unblockUI();就可以
+    setTimeout(function () {
+      $.unblockUI();
+    }, 500)
+  }
+};
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -112,12 +112,14 @@ export default {
     },
     list(page) {
       let _this = this;
+      Loading.show();
       _this.$refs.pagination.size = 5;
       // /admin 用于控台类的接口，/web 用于网站类的接口。接口设计中，用不同的请求前缀代表不同的入口，做接口隔离，方便做鉴权、统计、监控等
       _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",{
         page:page,
         size:_this.$refs.pagination.size,
       }).then((response) => {
+        Loading.hide();
         console.log("查询大章列表结果", response);
         let resp = response.data;
         _this.chapters = resp.content.list;
@@ -127,8 +129,10 @@ export default {
     },
     save(page) {
       let _this = this;
+      Loading.show();
       // /admin 用于控台类的接口，/web 用于网站类的接口。接口设计中，用不同的请求前缀代表不同的入口，做接口隔离，方便做鉴权、统计、监控等
       _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/save",_this.chapter).then((response) => {
+        Loading.hide();
         console.log("保存大章列表结果", response);
         let resp = response.data;
         if (resp.success){
@@ -150,8 +154,10 @@ export default {
         confirmButtonText: "确认!"
       }).then((result) => {
         if (result.isConfirmed) {
+          Loading.show();
           //restful 是一种请求风格。简单的理解：通过看url 就能知道这个请求是要对什么资源做什么操作
           _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) => {
+            Loading.hide();
             console.log("删除大章列表结果", response);
             let resp = response.data;
             if (resp.success){
