0abb0a75d93996325b789611417b81f57f7b3d70
zxy <zxy06291@gmail.com>
2023-12-24 08:24:13 +0800

    1.菜单隐藏掉大章和小节

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -109,7 +109,7 @@ export default {
     _this.course = course;
     _this.list(1);
     // sidebar 激活样式方法一
-    // this.$parent.activeSidebar("business-chapter-sidebar");
+    this.$parent.activeSidebar("business-course-sidebar");
   },
   methods:{
     /**
