8658cfb42bb93d94d6cf8cfdbdc6ed7e3a26cf94
zxy <zxy06291@gmail.com>
2023-12-25 01:15:16 +0800

    1.增加file模块，用于文件上传和存储

---
+++ file/src/main/java/com/course/file/config/FileApplication.java
@@ -0,0 +1,29 @@
+package com.course.file.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.server.mapper")
+public class FileApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileApplication.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(FileApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logger.info("启动成功！！");
+        logger.info("File地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
