0f4d2283bdb51eb0f9b5694fcd01135412d5d039
zxy <zxy06291@gmail.com>
2023-12-26 04:42:00 +0800

    1.文件上传功能开发：文件按用途分类保存

--- admin/src/components/file.vue
+++ admin/src/components/file.vue
@@ -22,6 +22,9 @@ export default {
     suffixs:{
       default: []
     },
+    use:{
+      default: ""
+    },
     afterUpload: {
       type: Function,
       default: null
@@ -53,6 +56,7 @@ export default {
       }
       //key:"file"必须和后端Controller参数名一致
       formData.append('file',file);
+      formData.append('use',_this.use);
       Loading.show();
       _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload",formData).then((response) => {
         Loading.hide();
--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -82,6 +82,7 @@
                  <file v-bind:id="'image-upload'"
                        v-bind:text="'上传头像'"
                        v-bind:suffixs="['jpg','jpeg', 'png']"
+                       v-bind:use="FILE_USE.TEACHER.key"
                        v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
@@ -132,6 +133,7 @@ export default {
       teacher: {},
       // teacher变量用于绑定form 表单的数据
       teachers: [],
+      FILE_USE:FILE_USE
     }
   },
   mounted: function () {
