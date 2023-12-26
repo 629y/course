0e12f6bf0c3bdcf02b9fcd73334c36adbf11e656
zxy <zxy06291@gmail.com>
2023-12-26 23:26:31 +0800

    1.大文件断点续传与极速秒传：增加big-file组件，分片传输测试成功

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -89,11 +89,11 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">视频</label>
                 <div class="col-sm-10">
-                  <file v-bind:input-id="'video-upload'"
-                        v-bind:text="'上传视频'"
+                  <big-file v-bind:input-id="'video-upload'"
+                        v-bind:text="'上传大视频'"
                         v-bind:suffixs="['mp4']"
                         v-bind:use="FILE_USE.COURSE.key"
-                        v-bind:after-upload="afterUpload"></file>
+                        v-bind:after-upload="afterUpload"></big-file>
                   <div v-show="section.video" class="row">
                     <div class="col-md-9">
                       <video v-bind:src="section.video" id="video" controls="controls"></video>
@@ -136,7 +136,7 @@
   import Pagination from "@/components/pagination.vue";
   import chapter from "@/views/admin/chapter.vue";
   import course from "@/views/admin/course.vue";
-  import File from "@/components/file.vue";
+  import BigFile from "@/components/big-file.vue";
   export default {
     name: "business-section",
     computed: {
@@ -147,7 +147,7 @@
         return course
       }
     },
-    components: {File, Pagination},
+    components: {BigFile, Pagination},
     data: function () {
       return {
         section:{},
