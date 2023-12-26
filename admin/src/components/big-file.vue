2224eaa28dcfe2b349170239ef362d4bed896573
zxy <zxy06291@gmail.com>
2023-12-27 07:01:00 +0800

    1.大文件断点续传与极速秒传：重构文件上传代码，将上传功能提取成一个单独的upload方法，使用递归调用upload方法，实现连续上传多个分片功能

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -34,7 +34,7 @@ export default {
     return {}
   },
   methods: {
-    uploadFile(){
+     uploadFile(){
       let _this = this;
       let formData = new window.FormData();
       let file = _this.$refs.file.files[0];
@@ -76,60 +76,65 @@ export default {
       //文件分片
       let shardSize = 10 * 1024 * 1024;   //以15MB为一个分片
       let shardIndex = 1;//分片索引,1表示第一个分片
-      //起始位置，其实就是和mysql分页一样
-      let start = (shardIndex - 1) * shardSize;//当前分片起始位置
-      //比如说35MB，第一个是20MB，第二个就是15MB，所以就要取最小的那个了。
-      let end = Math.min(file.size,start + shardSize);//当前分片结束位置
-      let fileShard = file.slice(start,end);//从文件中截取当前的分片数据
+
 
       let size = file.size;
       let shardTotal = Math.ceil(size / shardSize);//总片数
 
-      // //key:"shard"必须和后端Controller参数名一致
-      // formData.append('shard',fileShard);
-      // formData.append('shardIndex',shardIndex);
-      // formData.append('shardSize',shardSize);
-      // formData.append('shardTotal',shardTotal);
-      // formData.append('use',_this.use);
-      // formData.append('name',file.name);
-      // formData.append('suffix',suffix);
-      // formData.append('size',size);
-      // formData.append('key',key62);
-      // Loading.show();
-      // _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload",formData).then((response) => {
-      //   Loading.hide();
-      //   let resp = response.data;
-      //   console.log("上传文件成功：",resp);
-      //   _this.afterUpload(resp);
-      //   $("#" + _this.inputId + "-input").val("");
-      // });
-
+      let param = {
+        'shardIndex':shardIndex,
+        'shardSize':shardSize,
+        'shardTotal':shardTotal,
+        'use':_this.use,
+        'name':file.name,
+        'suffix':suffix,
+        'size':file.size,
+        'key':key62
+      };
+      _this.upload(param);
+    },
+    upload : function (param) {
+      let _this = this;
+      //shardIndex和shardTotal在param中已经有了，所以不需要单独传参
+      let shardIndex = param.shardIndex;
+      let shardTotal = param.shardTotal;
+      let shardSize = param.shardSize;
+      let fileShard = _this.getFileShard(shardIndex, shardSize);
       //将图片转为base64进行传输
       let fileReader = new FileReader();
-      fileReader.onload = function (e){
+      fileReader.onload = function (e) {
         let base64 = e.target.result;
         // console.log("base64",base64);
-        let param = {
-          'shard':base64,
-          'shardIndex':shardIndex,
-          'shardSize':shardSize,
-          'shardTotal':shardTotal,
-          'use':_this.use,
-          'name':file.name,
-          'suffix':suffix,
-          'size':file.size,
-          'key':key62
-        };
+
+        param.shard = base64;
+
         Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload",param).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload", param).then((response) => {
           Loading.hide();
           let resp = response.data;
-          console.log("上传文件成功：",resp);
-          _this.afterUpload(resp);
+          console.log("上传文件成功：", resp);
+          if (shardIndex < shardTotal) {
+            //上传下一个分片
+            param.shardIndex = param.shardIndex + 1;
+            _this.upload(param);
+            //递归，不断的重复做某一件事（上传分片），直到某个条件成立 (shardIndex == shardTotal) ,退出重复做的事。初学者慎用，容易陷入无限递归，跳不出来。
+          } else {
+            _this.afterUpload(resp);
+          }
           $("#" + _this.inputId + "-input").val("");
         });
       };
       fileReader.readAsDataURL(fileShard);
+    },
+    getFileShard: function (shardIndex, shardSize) {
+      let _this = this;
+      let file = _this.$refs.file.files[0];
+      //起始位置，其实就是和mysql分页一样
+      let start = (shardIndex - 1) * shardSize;//当前分片起始位置
+      //比如说35MB，第一个是20MB，第二个就是15MB，所以就要取最小的那个了。
+      let end = Math.min(file.size, start + shardSize);//当前分片结束位置
+      let fileShard = file.slice(start, end);//从文件中截取当前的分片数据
+      return fileShard;
     },
     selectFile(){
       let _this = this;
