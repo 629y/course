5d42151367db7bc0cf81d462536b49dea958dc35
zxy <zxy06291@gmail.com>
2023-12-28 04:51:43 +0800

    1.大文件断点续传与极速秒传：基于OSS接口实现文件上传，增加文件追加上传oss-append 和简单上传oss-simple

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -79,11 +79,11 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                 <big-file v-bind:input-id="'image-upload'"
+                 <file v-bind:input-id="'image-upload'"
                        v-bind:text="'上传头像'"
                        v-bind:suffixs="['jpg','jpeg', 'png']"
                        v-bind:use="FILE_USE.TEACHER.key"
-                       v-bind:after-upload="afterUpload"></big-file>
+                       v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -124,9 +124,10 @@
 <script>
 import Pagination from "../../components/pagination";
 import BigFile from "@/components/big-file.vue";
+import File from "@/components/file.vue";
 export default {
   name: "business-teacher",
-  components: {BigFile, Pagination},
+  components: {File, BigFile, Pagination},
   data: function () {
     return {
       teacher: {},
