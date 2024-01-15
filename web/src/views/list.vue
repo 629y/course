f45741c06f2217842fff294848454cd4e996159d
zxy <zxy06291@gmail.com>
2024-01-16 00:39:31 +0800

    1.课程列表页面开发，前端分页 2.增加分页组件，比admin简单，使用rem

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -3,6 +3,12 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row">
+          <div class="col-md-12">
+            <pagination ref="pagination" v-bind:list="listCourse"></pagination>
+          </div>
+        </div>
+        <br>
+        <div class="row">
           <div v-for="o in courses" class="col-md-4">
             <the-course v-bind:course="o"></the-course>
           </div>
@@ -15,10 +21,11 @@
 </template>
 <script>
 import TheCourse from "@/components/the-course.vue";
+import Pagination from "@/components/pagination.vue";
 
 export default {
   name: 'list',
-  components: {TheCourse},
+  components: {Pagination, TheCourse},
   data:function (){
     return{
       courses:[],
@@ -26,7 +33,8 @@ export default {
   },
   mounted() {
     let _this = this;
-    _this.listCourse();
+    _this.$refs.pagination.size = 1;
+    _this.listCourse(1);
   },
   methods:{
     /**
@@ -36,11 +44,12 @@ export default {
       let _this = this;
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list',{
         page:page,
-        size:3,
+        size:_this.$refs.pagination.size,
       }).then((response)=>{
         let resp = response.data;
         if (resp.success){
           _this.courses = resp.content.list;
+          _this.$refs.pagination.render(page,resp.content.total);
         }
       }).catch((response)=>{
         console.log("error:",response);
