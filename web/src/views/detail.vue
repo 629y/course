731143dbeb33379f792e702ccf30b027285a99c3
zxy <zxy06291@gmail.com>
2024-01-17 21:19:33 +0800

    1.课程详情页面开发，增加大章小节收缩和展开，注意：在v-for里写v-show，只修改属性不起作用，需要$set

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -40,27 +40,33 @@
               <div class="tab-pane active" id="info" v-html="course.content">
               </div>
               <div class="tab-pane" id="chapter">
-                <div v-for="chapter in chapters" class="chapter">
-                  <div class="chapter-chapter">
-                    <span class="folded-button">{{chapter.name}}</span>
+                <div v-for="(chapter,i) in chapters" class="chapter">
+                  <div v-on:click="doFolded(chapter,i)" class="chapter-chapter">
+                    <span>{{chapter.name}}</span>
+                    <span class="pull-right">
+                      <i v-show="chapter.folded" class="fa fa-plus-square" aria-hidden="true"></i>
+                      <i v-show="!chapter.folded" class="fa fa-minus-square" aria-hidden="true"></i>
+                    </span>
                   </div>
-                <table class="table table-striped">
-                  <tr v-for="(s,j) in chapter.sections" class="chapter-section-tr">
-                    <td class="col-sm-8 col-xs-12">
-                      <div class="section-title">
-                        <i class="fa fa-video-camera d-none d-sm-inline"></i>&nbsp;&nbsp;
-                        <!--d-none就是display:none,隐藏元素-->
-                        <span class="d-none d-sm-inline">第{{j+1}}节&nbsp;&nbsp;</span>
-                        {{s.title}}
-                        <span v-show="s.charge !== SECTION_CHARGE.CHARGE.key"
-                              class="badge badge-primary hidden-xs">免费</span>
-                      </div>
-                    </td>
-                    <td class="col-sm-1 text-right">
-                      <span class="badge badge-primary">{{s.time | formatSecond}}</span>
-                    </td>
-                  </tr>
-                </table>
+                  <div v-show="!chapter.folded">
+                    <table class="table table-striped">
+                      <tr v-for="(s,j) in chapter.sections" class="chapter-section-tr">
+                        <td class="col-sm-8 col-xs-12">
+                          <div class="section-title">
+                            <i class="fa fa-video-camera d-none d-sm-inline"></i>&nbsp;&nbsp;
+                            <!--d-none就是display:none,隐藏元素-->
+                            <span class="d-none d-sm-inline">第{{j+1}}节&nbsp;&nbsp;</span>
+                            {{s.title}}
+                            <span v-show="s.charge !== SECTION_CHARGE.CHARGE.key"
+                                  class="badge badge-primary hidden-xs">免费</span>
+                          </div>
+                        </td>
+                        <td class="col-sm-1 text-right">
+                          <span class="badge badge-primary">{{s.time | formatSecond}}</span>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
@@ -125,6 +131,17 @@ export default {
         }
       })
     },
+    /**
+     * 展开/收缩一个章节
+     * @param chapter
+     * @param i
+     */
+    doFolded(chapter,i){
+      let _this = this;
+      chapter.folded = !chapter.folded;
+      //在v-for里写v-show,只修改属性不起作用，需要$set
+      _this.$set(_this.chapters,i,chapter);
+    },
   }
 }
 </script>
@@ -157,6 +174,7 @@ export default {
     padding:1.25rem;
     background-color: #23527c;
     color:white;
+    cursor:pointer;
   }
   .chapter-section-tr{
     font-size: 1rem;
